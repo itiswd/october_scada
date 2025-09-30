3ac031708964fc838cb52765ed1053e7e47d88d8
itiswd <ibrahimtharwat3344@gmail.com>
2025-09-30 22:09:30 +0300

    fix: Correct index for Station 7 in bottom navigation bar

--- lib/presentation/pages/main_navigation_page.dart
+++ lib/presentation/pages/main_navigation_page.dart
@@ -71,7 +71,7 @@ class _MainNavigationPageState extends ConsumerState<MainNavigationPage> {
               _buildNavItem(3, 'Station 4', isMobile),
               _buildNavItem(4, 'Station 5', isMobile),
               _buildNavItem(5, 'Station 6', isMobile),
-              _buildNavItem(5, 'Station 7', isMobile),
+              _buildNavItem(6, 'Station 7', isMobile),
             ],
           ),
         ),
